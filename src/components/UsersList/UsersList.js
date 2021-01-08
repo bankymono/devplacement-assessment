fd69afc7046e4e33bab0c3ffa27691e8a701b2d8
bankymono <bankymono@gmail.com>
2021-01-08 09:08:12 +0100

    created the structure of the key ui components

---
+++ src/components/UsersList/UsersList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import User from '../User/User'
+
+const Users = () => {
+    return (
+        <div>
+            users list
+            <User />       
+        </div>
+    )
+}
+
+export default Users
